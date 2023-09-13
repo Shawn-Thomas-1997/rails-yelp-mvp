6c25f301f358b7deed22b95801594f42b76e8963
Shawn Thomas <shawnt41@gmail.com>
2023-09-13 14:08:20 +0200

    Yelp review rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/:id", to: "restaurants#show"
+  get "restaurants/new", to: "restaurants#new"
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do |i|
+  restaurant = Restaurant.new(
+    name: "Restaurant #{i + 1}",
+    address: "Address #{i + 1}",
+    category: categories.sample
+  )
+  if restaurant.valid?
+    restaurant.save
+    puts "Created restaurant: #{restaurant.name}"
+  else
+    puts "Failed to create restaurant: #{restaurant.name}"
+  end
+end
